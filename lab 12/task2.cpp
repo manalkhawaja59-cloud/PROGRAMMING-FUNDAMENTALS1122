0df375661170da8b45af126f4ef66d3e2e1322dd
manalkhawaja59-cloud <manalkhawaja59@gmail.com>
2026-01-21 19:33:45 +0500

    edited file

---
+++ lab 12/task2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+// Function with no return and no parameters
+void displayGreeting() {
+    cout << "Hello, Welcome to C++ Functions!" << endl;
+}
+
+int main() {
+    displayGreeting(); // Function call
+    return 0;
+}
